4407a39e2c5f29a651b2fa2966c16826b4690193
hainguyen81 <hainguyen81@users.noreply.github.com>
2023-12-19 15:46:34 +0700

    read GIT token from file

--- .docker/my_build_git_docker.bat
+++ .docker/my_build_git_docker.bat
@@ -1,3 +1,6 @@
 @echo off
 
-build_git_docker.bat hainguyen81 <GIT TOKEN> master
+SET /p GIT_TOKEN=<git.token
+echo [ ------- GIT_TOKEN: %GIT_TOKEN% ------- ]
+
+build_git_docker.bat hainguyen81 %GIT_TOKEN% master
